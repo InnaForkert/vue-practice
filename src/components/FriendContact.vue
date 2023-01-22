43c1ff255c665fda0fb42436bed7f0ccf37a642e
Inna <inna.forkert@gmail.com>
2023-01-22 02:54:24 +0200

    basic app

---
+++ src/components/FriendContact.vue
@@ -0,0 +1,30 @@
+<template>
+  <li>
+    <h2>{{ friend.name }}</h2>
+    <button @click="toggleDetails">
+      {{ detailsAreVisible ? "Hide Details" : "Show Details" }}
+    </button>
+    <ul v-if="detailsAreVisible">
+      <li><strong>Phone:</strong>{{ friend.phone }}</li>
+      <li><strong>Email:</strong>{{ friend.email }}</li>
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      detailsAreVisible: false,
+      friend: { id: "1", name: "Inna", phone: "123456", email: "hi@gmail.com" },
+    };
+  },
+  methods: {
+    toggleDetails() {
+      this.detailsAreVisible = !this.detailsAreVisible;
+    },
+  },
+};
+</script>
+
+<style scoped></style>
